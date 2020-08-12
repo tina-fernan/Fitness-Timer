2ffac225936b2d81004c41ec4b6077878f7eb983
tina-fernan <tinaaroom@gmail.com>
2020-08-12 10:54:02 +0200

    update methods

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Insomnia } from '@ionic-native/insomnia/ngx'
 
 @Component({
   selector: 'app-home',
@@ -16,14 +17,28 @@ export class HomePage {
   minutes: number = 1;
   seconds: any = 30;
 
-  
+  elapsed: any ={
+    h: '00',
+    m: '00',
+    s: '00',
 
-  constructor() {}
+  }
+  overallTimer: any = false;
+
+  
+  constructor(private insomnia: Insomnia) {
+
+
+  }
 
   startTime(){
 
   if(this.timer){
     clearInterval(this.timer)
+  }
+  if(!this.overallTimer){
+    this.progressTimer();
+    this.insomnia.keepAwake();
   }
 
     this.timer = false;
@@ -47,5 +62,48 @@ export class HomePage {
     },1000)
 
   }
+  progressTimer(){
+    let countDownDate = new Date();
+    
+    this.overallTimer = setInterval(() => {
+      
+      let now = new Date().getTime();
+      let distance = now - countDownDate.getTime();
+
+      this.elapsed.h = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      this.elapsed.m = Math.floor((distance % (1000 * 60 * 60 )) / (1000 * 60 ));
+      this.elapsed.s = Math.floor((distance % (1000 * 60 )) / (1000));
+
+      this.elapsed.h = this.pad(this.elapsed.h ,2 );
+      this.elapsed.m = this.pad(this.elapsed.m ,2 );
+      this.elapsed.s = this.pad(this.elapsed.s ,2 );
+
+
+    })
+
+  }
+  pad(num , size){
+    let s = num + "";
+    while(s.length < size) s= "0" +s;
+    return s;
+  }
+
+  stopTime(){
+
+  clearInterval(this.timer);
+  clearInterval(this.overallTimer);
+  this.overallTimer = false;
+  this.timer = false;
+  this.percent = 0;
+  this.progress = 0;
+  this.elapsed = {
+    h:'00',
+    m:'00',
+    s:'00'
+  }
+  this.insomnia.allowSleepAgain()
+
+  }
+
 
 }
